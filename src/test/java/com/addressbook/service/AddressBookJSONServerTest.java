6ffd3f28bcd326ac46b08aa851c5ec364c8df771
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-03-06 23:00:20 +0530

    [Sarthak]: modified AddressBookJSONServerTest for UC23

--- src/test/java/com/addressbook/service/AddressBookJSONServerTest.java
+++ src/test/java/com/addressbook/service/AddressBookJSONServerTest.java
@@ -1,6 +1,6 @@
 package com.addressbook.service;
 
-import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.*;
 
 import java.util.List;
 
@@ -23,6 +23,46 @@ public class AddressBookJSONServerTest {
 		System.out.println("Contacts retrieved: " + contacts.size());
 
 		contacts.forEach(contact -> System.out.println(contact.getFirstName()));
+	}
+
+	@Test
+	public void givenMultipleContacts_whenAdded_shouldSyncWithJSONServer() {
+
+		Contact contact1 = new Contact("Aman", "Singh", "Delhi", "Delhi", "DL", "110001", "9999991111",
+				"aman@test.com");
+
+		Contact contact2 = new Contact("Rohit", "Verma", "Indore", "Indore", "MP", "452001", "9999992222",
+				"rohit@test.com");
+
+		List<Contact> contacts = List.of(contact1, contact2);
+
+		contacts.forEach(contact -> {
+
+			Response response =
+			        given()
+			                .contentType("application/json")
+			                .body(contact)
+			        .when()
+			                .post("http://localhost:3000/contacts");
+
+			response.then().statusCode(201);
+
+			System.out.println("Added contact: " + contact.getFirstName());
+		});
+	}
+
+	@Test
+	public void whenContactsRetrieved_shouldMatchJSONServerCount() {
+
+		Response response = get("http://localhost:3000/contacts");
+
+		response.then().statusCode(200);
+
+		List<Contact> contacts = response.jsonPath().getList("", Contact.class);
+
+		System.out.println("Total contacts in JSONServer: " + contacts.size());
+
+		contacts.forEach(c -> System.out.println(c.getFirstName()));
 	}
 
 }
