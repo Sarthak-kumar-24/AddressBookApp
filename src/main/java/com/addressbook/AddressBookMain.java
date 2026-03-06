ebc3bb079c8e432949412ae2431ace047756b509
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-03-06 20:51:31 +0530

    [Sarthak]: modified AddressBookMain for UC16

---
+++ src/main/java/com/addressbook/AddressBookMain.java
@@ -0,0 +1,28 @@
+package com.addressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class of the Spring Boot Address Book Application.
+ *
+ * @SpringBootApplication does three things:
+ * 1. Enables component scanning
+ * 2. Enables auto configuration
+ * 3. Enables Spring Boot configuration
+ *
+ * This class starts the Spring Boot application.
+ */
+@SpringBootApplication
+public class AddressBookMain {
+
+    /**
+     * Main method where program execution begins.
+     */
+    public static void main(String[] args) {
+
+    	 SpringApplication.run(AddressBookMain.class, args);
+
+         System.out.println("Address Book Spring Boot Application Started Successfully!");
+    }
+}
