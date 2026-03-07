461651f938cf3755f93030056ceccfb63caf1aa8
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-03-07 16:38:13 +0530

    [Sarthak]: modified Contact for UC19

--- src/main/java/com/addressbook/entity/Contact.java
+++ src/main/java/com/addressbook/entity/Contact.java
@@ -11,6 +11,13 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  * Contact class represents a single person in the Address Book.
@@ -25,6 +32,14 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "contacts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode(of = "id")
+
 public class Contact {
 
 	/**
@@ -47,148 +62,7 @@ public class Contact {
 	@ManyToOne
 	@JoinColumn(name = "address_book_id")
 	private AddressBook addressBook;
-	
 
-    /**
-     * Default constructor required by JPA
-     */
-    public Contact() {
-    }
-
-	public Contact(String firstName, String lastName, String address, String city, String state, String zip,
-			String phoneNumber, String email) {
-
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.address = address;
-		this.city = city;
-		this.state = state;
-		this.zip = zip;
-		this.phoneNumber = phoneNumber;
-		this.email = email;
-	}
-	
-	@PrePersist
-    public void setDateAdded() {
-        this.dateAdded = LocalDate.now();
-    }
-	
-	public LocalDate getDateAdded() {
-	    return dateAdded;
-	}
-
-	public void setDateAdded(LocalDate dateAdded) {
-	    this.dateAdded = dateAdded;
-	}
-
-	
-
-	// setters (needed for UC-3)
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public String getCity() {
-	    return city;
-	}
-
-	public String getState() {
-	    return state;
-	}
-	public String getAddress() { 
-		return address; 
-	}
-
-	// getters
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public String getLastName() {
-	    return lastName;
-	}
-	public String getZip() {
-	    return zip;
-	}
-	public String getPhoneNumber() { 
-		return phoneNumber; 
-	}
-	public String getEmail() { 
-		return email; 
-	}
-	public Long getId() {
-		return id;
-	}
-	
-	
-	public AddressBook getAddressBook() {
-	    return addressBook;
-	}
-
-	public void setAddressBook(AddressBook addressBook) {
-	    this.addressBook = addressBook;
-	}
-	
-    /**
-     * UC7:
-     * Two contacts are considered equal if
-     * they have the same first name and last name.
-     */
-	@Override
-	public boolean equals(Object obj) {
-
-	    if (this == obj)
-	        return true;
-
-	    if (obj == null || getClass() != obj.getClass())
-	        return false;
-
-	    Contact contact = (Contact) obj;
-
-	    return id.equals(contact.id) &&
-	            firstName.equals(contact.firstName) &&
-	            lastName.equals(contact.lastName) &&
-	            city.equals(contact.city) &&
-	            state.equals(contact.state) &&
-	            zip.equals(contact.zip) &&
-	            phoneNumber.equals(contact.phoneNumber) &&
-	            email.equals(contact.email);
-	}
-    
-    /**
-     * hashCode overridden to maintain contract with equals().
-     */
-	@Override
-	public int hashCode() {
-	    return Objects.hash(id, firstName, lastName, city, state, zip, phoneNumber, email);
-	}
 
 	/**
 	 * Displays all contact details in a readable format. Used after adding or
@@ -204,14 +78,5 @@ public class Contact {
 		System.out.println("Phone No.   : " + phoneNumber);
 		System.out.println("Email       : " + email);
 	}
-	@Override
-	public String toString() {
-	    return "Name: " + firstName + " " + lastName +
-	            ", Address: " + address +
-	            ", City: " + city +
-	            ", State: " + state +
-	            ", Zip: " + zip +
-	            ", Phone: " + phoneNumber +
-	            ", Email: " + email;
-	}
+
 }
